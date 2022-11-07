28bdbaba6d66fc7bbd7885ef197ead253fb208db
Eugene Zorin <112591311+EugeneZorin@users.noreply.github.com>
2022-11-07 20:00:03 +0300

    new update 07.11.2022 Создан интерфейс первый страницы

---
+++ app/src/main/java/com/example/invertedword/ReverseActivity.kt
@@ -0,0 +1,4 @@
+package com.example.invertedword
+
+class ReverseActivity {
+}
--- app/src/main/java/com/example/invertedword/screens/MainScreen.kt
+++ app/src/main/java/com/example/invertedword/screens/MainScreen.kt
@@ -1,12 +1,16 @@
 package com.example.invertedword.screens
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.SemanticsProperties.Text
+import androidx.compose.ui.text.input.KeyboardType.Companion.Text
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 
 @Preview(showBackground = true)
@@ -18,5 +22,49 @@ fun MainScreen() {
             .padding(5.dp),
             ) {
 
+        Box(modifier = Modifier
+            .width(250.dp)
+            .height(60.dp)
+            .offset(x = 73.dp, y = 150.dp)
+        ) { ReversText()}
+
+        Box(modifier = Modifier
+            .width(250.dp)
+            .height(60.dp)
+            .offset(x = 73.dp, y = 250.dp)
+        ) { ExclusionsText() }
+
+        Box(modifier = Modifier
+            .width(123.dp)
+            .height(51.dp)
+            .offset(x = 125.dp, y = 350.dp)
+        ) {
+            Button(onClick = { /*TODO*/ }) {
+                Text("Реверс", fontSize = 25.sp)
+            }
+        }
+
     }
+}
+
+@Composable
+fun ReversText() {
+    var revers by remember { mutableStateOf("") }
+
+    OutlinedTextField(
+        value = revers,
+        onValueChange = { revers = it },
+        label = { Text("Инверсия") }
+    )
+}
+
+@Composable
+fun ExclusionsText(){
+    var exclusions by remember { mutableStateOf("") }
+
+    OutlinedTextField(
+        value = exclusions,
+        onValueChange = {exclusions = it},
+        label = { Text("Исключения")}
+    )
 }
